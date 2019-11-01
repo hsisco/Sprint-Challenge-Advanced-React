798f478142f2250c680d3149b2a346488a7c416a
hsisco <hysensisco@gmail.com>
2019-11-01 10:36:46 -0700

    cDM on PlayersPage

--- client/src/components/PlayersPage.js
+++ client/src/components/PlayersPage.js
@@ -8,16 +8,32 @@ export class PlayersPage extends React.Component {
         player: [],
       };
   }
-  return (
-    <div className="soccerLadies">
-      {players.map(soccerLady => (
-        <PlayerCard
-          key={soccerLady.name}
-          soccerLady={soccerLady}
-          country={soccerLady.country}
-          searches={soccerLady.searches} />
-      ))}
-    </div>
-  );
+
+  componentDidMount() {
+    console.log('cDM: fetching players data');
+
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        this.setState({
+            player: res.data
+        });
+        console.log(res)
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div className="soccerLadies">
+        {players.map(soccerLady => (
+          <PlayerCard
+            key={soccerLady.name}
+            soccerLady={soccerLady}
+            country={soccerLady.country}
+            searches={soccerLady.searches} />
+        ))}
+      </div>
+    );
+  }
 }
 
